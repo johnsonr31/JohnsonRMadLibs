c81d023292308ee86bc3fba28bb160449c31e4a3
ricRobo4 <artistr3@proton.me>
2022-10-18 16:09:46 -0700

    added a comment and another input

--- Program.cs
+++ Program.cs
@@ -7,6 +7,7 @@ bool playAgain = true;
 // This while loop will continue running as long as playAgain is true
 while(playAgain == true)
 {
+    // This will print some words to the console, and take the user's input
     Console.WriteLine("Do you want to create a story?");
     Console.Write("YES or NO: ");
     string yesNo = Console.ReadLine();
@@ -22,5 +23,8 @@ while(playAgain == true)
             Console.Write("Please enter a name: ");
             string name = Console.ReadLine();
             name = name.ToUpper();
+
+            Console.WriteLine(" ");
+            Console.Write("Enter a color");
         }
 }
