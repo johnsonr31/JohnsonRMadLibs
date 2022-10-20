fe042ff65517c9765ec703019e46ef980d45aea1
ricRobo4 <artistr3@proton.me>
2022-10-20 15:55:21 -0700

    added else statement, added readme

--- Program.cs
+++ Program.cs
@@ -3,87 +3,97 @@
 //
 
 bool playAgain = true;
+bool isNum;
+int num;
 
+
+
+    // This will print some words to the console
+    Console.WriteLine("Do you want to create a story?");
 // This while loop will continue running as long as playAgain is true
 while(playAgain == true)
 {
-    // This will print some words to the console, and take the user's input
-    Console.WriteLine("Do you want to create a story?");
+    // This will take the user's input to see if they want to start
     Console.Write("YES or NO: ");
     // The user's input will be taken, and it will be converted to all uppercase
     string yesNo = Console.ReadLine();
     yesNo = yesNo.ToUpper();
-        if(yesNo == "NO")
+    isNum = Int32.TryParse(yesNo, out num);
+        if(yesNo == "NO" && isNum != true)
         {
             Console.WriteLine("Then goodbye.");
             playAgain = false;
         }
-        else if(yesNo == "YES")
+        else if(yesNo == "YES" && isNum != true)
         {
             Console.WriteLine("Alright");
             Console.Write("Please enter a name: ");
             string name1 = Console.ReadLine();
-            name1 = name1.ToUpper();
+
 
             Console.WriteLine(" ");
             Console.Write("Enter a color: ");
             string color1 = Console.ReadLine();
-            color1 = color1.ToUpper();
 
             Console.WriteLine(" ");
             Console.Write("Enter a noun: ");
             string noun1 = Console.ReadLine();
-            noun1 = noun1.ToUpper();
 
             Console.WriteLine("");
             Console.Write("Enter an animal: ");
             string animal1 = Console.ReadLine();
-            animal1 = animal1.ToUpper();
 
             Console.WriteLine("");
             Console.Write("Enter another name: ");
             string name2 = Console.ReadLine();
-            name2 = name2.ToUpper();
             
             Console.WriteLine("");
             Console.Write("Enter a noun: ");
             string noun2 = Console.ReadLine();
-            noun2 = noun2.ToUpper();
 
             Console.WriteLine("");
             Console.Write("Enter a verb: ");
             string verb1 = Console.ReadLine();
-            verb1 = verb1.ToUpper();
             
             Console.WriteLine("");
             Console.Write("Enter a location: ");
             string location1 = Console.ReadLine();
-            location1 = location1.ToUpper();
 
             Console.WriteLine("");
             Console.Write("Enter a country: ");
             string country = Console.ReadLine();
-            country = country.ToUpper();
 
             Console.WriteLine("");
             Console.Write("Enter an adjective: ");
             string adjective1 = Console.ReadLine();
-            adjective1 = adjective1.ToUpper();
 
             Console.WriteLine("");
-            Console.Write("Enter an animal: ");
+            Console.Write("Enter an animal (plural): ");
             string animal2 = Console.ReadLine();
-            animal2 = animal2.ToUpper();
             
             Console.WriteLine("");
-            Console.Write("Enter an verb: ");
+            Console.Write("Enter an verb ending with -ing: ");
             string verb2 = Console.ReadLine();
-            verb2 = verb2.ToUpper();
 
             Console.WriteLine("");
-            Console.Write("Enter an adjective: ");
+            Console.Write("Enter an adjective ending with -ly: ");
             string adjective2 = Console.ReadLine();
-            adjective2 = adjective2.ToUpper();
 
+
+
+        Console.WriteLine($"There was once a person named {name1} who lived in a {color1} {noun1} out in the woods. One day, ");
+        Console.WriteLine($"{name1} woke up to find a {animal1} knocking at his door. The {animal1} was named {name2}, ");
+        Console.WriteLine($"and was here to tell {name1} about a magic {noun2} that could {verb1}. {name1} agreed to find ");
+        Console.WriteLine($"the {noun2}, and {name2} helped guide him. They would travel to the {location1} which was in {country}, ");
+        Console.WriteLine($"where they would encounter a group of {adjective1} {animal2}. The {animal2} were also looking for ");
+        Console.WriteLine($"{noun2}, and held {name1} and {name2} captive. {name1} and {name2} managed to {verb2} to escape ");
+        Console.WriteLine($"from them, and managed to reach the {location1}. They got in and found the magic {noun2}, and lived ");
+        Console.WriteLine($"{adjective2} ever after.");
+        Console.WriteLine(" ");
+        Console.WriteLine("Do you want to go again?");
+        }
+        else
+        {
+            Console.WriteLine("Invalid Input.");
         }
 }
--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # RJohnsonMadLibs
 Richard Johnson
 Last edited 10/18/2022
+
+This program is a mad libs game, and will ask the user for various words, such as nouns, adjectives, verbs, and more specific words like proper nouns or countries. It will then print these words to the console in a story which might not make much sense depending on what words the user typed
+
+Peer Review: 
